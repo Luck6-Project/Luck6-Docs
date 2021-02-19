742f5f279ecd38a11489a8e3f26a7edfdc9ec598
shx27821 <62169872+shx27821@users.noreply.github.com>
2021-02-19 16:49:05 +0800

    Update github_commit_update.py

--- github_commit_update.py
+++ github_commit_update.py
@@ -6,14 +6,14 @@ main = json.loads(get("https://api.github.com/repos/Luck6-Project/Luck6-Docs/com
 text = "#update #docs #" + main['commit']['author']['name'].replace('_', '') + \
        ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/Luck6-Project/Luck6-Docs/commit/' + \
        main['sha'] + '): ' + main['commit']['message']
-push_content = {'chat_id': '-1001334021647, 'disable_web_page_preview': 'True', 'parse_mode': 'markdown',
+push_content = {'chat_id': '-1001341765556', 'disable_web_page_preview': 'True', 'parse_mode': 'markdown',
                 'text': text}
 url = 'https://api.telegram.org/bot' + token + '/sendMessage'
 try:
     main_req = post(url, data=push_content)
 except:
     pass
-push_content['chat_id'] = '-1001334021647'
+push_content['chat_id'] = '-1001341765556'
 time.sleep(1)
 try:
     main_req = get(url, data=push_content)
